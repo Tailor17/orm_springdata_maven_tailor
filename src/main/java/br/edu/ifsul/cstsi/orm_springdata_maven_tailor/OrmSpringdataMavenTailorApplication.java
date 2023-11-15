3f9ec596681800527313a588d99116d635be3d6c
Tailor17 <tailorzitzke.pl013@academico.ifsul.edu.br>
2023-11-15 13:54:37 -0300

    Objetivo 5

---
+++ src/main/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/OrmSpringdataMavenTailorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.orm_springdata_maven_tailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmSpringdataMavenTailorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmSpringdataMavenTailorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/OrmSpringdataMavenTailorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.orm_springdata_maven_tailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmSpringdataMavenTailorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
