723d6aeb92d38b46d83aea3ccc5e7e5ec73c79a3
Tailor17 <tailorzitzke.pl013@academico.ifsul.edu.br>
2023-11-22 19:56:46 -0300

    Objetivo 5(Final)

--- src/main/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/model/Processo.java
+++ src/main/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/model/Processo.java
@@ -17,12 +17,12 @@ public class Processo{
     private  String descricaoProcesso;
     private Situacao situacaoProcesso;
 
-    @OneToMany
+    @OneToMany(mappedBy = "processo")
     private List<Custa> custas = new ArrayList<>();
-    @OneToMany
+    @OneToMany(mappedBy = "processo")
     private List<Audiencia>audiencias = new ArrayList<>();
-    @OneToMany
-    private List<Vara> varas = new ArrayList<>();
+    @ManyToOne
+    private Vara vara;
 
     public Integer registrarProcesso() {
         return null;
@@ -40,96 +40,5 @@ public class Processo{
         return null;
     }
 
-    public Processo() {
-        super();
-    }
 
-    public Processo(Long numeroProcesso, LocalDate aberturaProcesso, LocalDate conclusaoProcesso, String descricaoProcesso, Situacao situacaoProcesso, List<Custa> custas, List<Audiencia> audiencias, List<Vara> varas) {
-        this.numeroProcesso = numeroProcesso;
-        this.aberturaProcesso = aberturaProcesso;
-        this.conclusaoProcesso = conclusaoProcesso;
-        this.descricaoProcesso = descricaoProcesso;
-        this.situacaoProcesso = situacaoProcesso;
-        this.custas = custas;
-        this.audiencias = audiencias;
-        this.varas = varas;
-    }
-
-    public Long getNumeroProcesso() {
-        return numeroProcesso;
-    }
-
-    public void setNumeroProcesso(Long numeroProcesso) {
-        this.numeroProcesso = numeroProcesso;
-    }
-
-    public LocalDate getAberturaProcesso() {
-        return aberturaProcesso;
-    }
-
-    public void setAberturaProcesso(LocalDate aberturaProcesso) {
-        this.aberturaProcesso = aberturaProcesso;
-    }
-
-    public LocalDate getConclusaoProcesso() {
-        return conclusaoProcesso;
-    }
-
-    public void setConclusaoProcesso(LocalDate conclusaoProcesso) {
-        this.conclusaoProcesso = conclusaoProcesso;
-    }
-
-    public String getDescricaoProcesso() {
-        return descricaoProcesso;
-    }
-
-    public void setDescricaoProcesso(String descricaoProcesso) {
-        this.descricaoProcesso = descricaoProcesso;
-    }
-
-    public Situacao getSituacaoProcesso() {
-        return situacaoProcesso;
-    }
-
-    public void setSituacaoProcesso(Situacao situacaoProcesso) {
-        this.situacaoProcesso = situacaoProcesso;
-    }
-
-    public List<Custa> getCustas() {
-        return custas;
-    }
-
-    public void setCustas(List<Custa> custas) {
-        this.custas = custas;
-    }
-
-    public List<Audiencia> getAudiencias() {
-        return audiencias;
-    }
-
-    public void setAudiencias(List<Audiencia> audiencias) {
-        this.audiencias = audiencias;
-    }
-
-    public List<Vara> getVaras() {
-        return varas;
-    }
-
-    public void setVaras(List<Vara> varas) {
-        this.varas = varas;
-    }
-
-    @Override
-    public String toString() {
-        return "\nProcesso{" +
-                "numeroProcesso=" + numeroProcesso +
-                ", aberturaProcesso=" + aberturaProcesso +
-                ", conclusaoProcesso=" + conclusaoProcesso +
-                ", descricaoProcesso='" + descricaoProcesso + '\'' +
-                ", situacaoProcesso=" + situacaoProcesso +
-                ", custas=" + custas +
-                ", audiencias=" + audiencias +
-                ", varas=" + varas +
-                '}';
-    }
 }
--- src/main/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/model/Vara.java
+++ src/main/java/br/edu/ifsul/cstsi/orm_springdata_maven_tailor/model/Vara.java
@@ -2,6 +2,8 @@ package br.edu.ifsul.cstsi.orm_springdata_maven_tailor.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity(name = "Vara")
 @Table(name = "Vara")
 public class Vara {
@@ -9,8 +11,8 @@ public class Vara {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Idvara;
     private String descricaoVara;
-    @ManyToOne
-    private Processo processo;
+    @OneToMany(mappedBy = "vara")
+    private List<Processo> processos;
 
     public String ConsultaVara() {
         return null;
@@ -20,46 +22,5 @@ public class Vara {
         return null;
     }
 
-    public Vara() {
-        super();
-    }
 
-    public Vara(Long idvara, String descrcaoVara, Processo processo) {
-        Idvara = idvara;
-        this.descricaoVara = descrcaoVara;
-        this.processo = processo;
-    }
-
-    public Long getIdvara() {
-        return Idvara;
-    }
-
-    public void setIdvara(Long idvara) {
-        Idvara = idvara;
-    }
-
-    public String getDescircaoVara() {
-        return descricaoVara;
-    }
-
-    public void setDescricaoVara(String descircaoVara) {
-        this.descricaoVara = descricaoVara;
-    }
-
-    public Processo getProcesso() {
-        return processo;
-    }
-
-    public void setProcesso(Processo processo) {
-        this.processo = processo;
-    }
-
-    @Override
-    public String toString() {
-        return "\nVara{" +
-                "Idvara=" + Idvara +
-                ", descrcaoVara='" + descricaoVara + '\'' +
-                ", processo=" + processo +
-                '}';
-    }
 }
